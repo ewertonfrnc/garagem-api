7f068e488179eb1867f4b5638967bdba6ed770f0
Ewerton Franco <ewefsilva@gmail.com>
2024-09-18 15:25:50 -0300

    Enable CORS in main application
    Added app.enableCors() to the bootstrap function in main.ts to allow cross-origin resource sharing. This change is necessary to support applications making requests from different origins.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+
   await app.listen(8000);
 }
 bootstrap();
